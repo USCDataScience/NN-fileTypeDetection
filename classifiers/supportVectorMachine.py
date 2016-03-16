8a6a2cb42374a98d234ca605f2610cd33f670d09
Rahul Ravindran <rahulravindran0108@gmail.com>
2016-03-15 19:30:27 -0700

    feat: adding support vector machine tuning parameters and adding support grid search.

--- classifiers/supportVectorMachine.py
+++ classifiers/supportVectorMachine.py
@@ -1,5 +1,6 @@
 from sklearn import svm
 from sklearn.metrics import accuracy_score
+from sklearn import grid_search
 
 class SupportVectorMachineClassifier:
 
@@ -9,6 +10,42 @@ class SupportVectorMachineClassifier:
 		"""
 		self.outputHeader = "#svm"
 		self.clf = None
+		self.kernel = "rbf"
+		self.C = None
+		self.tol = 1e-3
+
+	def setKernel(self, param):
+		"""
+		This function sets the kernel parameter of the Support Vector Machine
+		"""
+		if param in ["rbf","linear","poly","sigmoid"]:
+			self.kernel = param
+		else:
+			print "error in parameter, defaulting to rbf kernel"
+
+	def setTol(self, param):
+		"""
+		This function is used to set the tolerance value of the SVM
+		"""
+		if type(param) is float:
+			self.tol = param
+		else:
+			print "there was an error in the parameter value, defaulting to 0.001"
+
+	def setC(self, param):
+		"""
+		This function sets the C value for the SVM Classifier
+		"""
+		self.C = param
+
+	def tuneParameter(self, parameters):
+		"""
+		This function is used for tuning the parameter of SVM.
+
+		example:
+		parameters = {'kernel':('linear', 'poly','rbf'), 'C':[1, 10]}
+		"""
+		self.clf = grid_search.GridSearchCV(svm.SVC(), parameters)
 
 	def buildModel(self):
 		"""
@@ -20,7 +57,7 @@ class SupportVectorMachineClassifier:
 		"""
 		Training the Support Vector Machine
 		"""
-		self.clf.fit(X, Y)
+		print self.clf.fit(X, Y)
 
 	def validateSVM(self,X, Y):
 		"""
