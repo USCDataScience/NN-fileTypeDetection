860f7ad21892869cc29c6c249c88e74b2792a626
Rahul Ravindran <rahulravindran0108@gmail.com>
2016-04-06 21:46:28 -0700

    feat: adding logging and parameter tuning to Stochastic GDC

--- classifiers/stochasticgradientDescentClassifier.py
+++ classifiers/stochasticgradientDescentClassifier.py
@@ -1,5 +1,6 @@
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import accuracy_score
+import logging
 
 class GDClassifier:
 
@@ -9,12 +10,44 @@ class GDClassifier:
         """
         self.header = "#gdc"
         self.clf = None
+        self.loss = "hinge" #default value
+        self.penalty = "l2" #deafult value
+        self.acceptedLossValues = ["hinge", "log", "modified_huber",
+         "squared_hinge", "perceptron"]
+        self.acceptedPenaltyValues = [ "none", "l2", "l1", "elasticnet"]
+
+    def setLoss(self, loss):
+        """
+        Sets the loss parameter for the SGDC
+        """
+        try:
+            if loss in self.acceptedLossValues:
+                self.loss = loss
+            else:
+                raise ValueError("Error in input value")
+        except Exception as error:
+            logging.warning("Error: No such loss value:%s", loss)
+
+    def setPenalty(self, penalty):
+        """
+        Sets the penalty for the SGDC classifier. 
+        This is the regularization value
+        """
+        try:
+            if penalty in self.acceptedPenaltyValues:
+                self.penalty = penalty
+            else:
+                raise ValueError("Error in input value")
+        except Exception as error:
+            logging.warning("Error: no such penalty value:%s", penalty)
 
     def buildModel(self):
         """
         This builds the model of the Gradient Descent Classifier
         """
+        logging.info("Building Model")
         self.clf = SGDClassifier(loss="hinge", penalty="l2")
+        logging.info("Finished Building Model")
 
     def trainGDC(self,X, Y):
         """
